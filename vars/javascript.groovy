7b7abca891caa4732f7445b8f9e729c6b99a7474
[ravali]git config --global user.email [ravali.anthati@gmail.com] <ravali.anthati@gmail.com>
2020-10-13 12:09:13 -0700

    new

--- vars/javascript.groovy
+++ vars/javascript.groovy
@@ -2,14 +2,14 @@ def call(Map config=[:], Closure body) {
     node {
         git url: "https://github.com/werne2j/sample-nodejs"
         stage("Install") {
-            bat "npm install"
+            sh "npm install"
         }
         stage("Test") {
-            bat "npm test"
+            sh "npm test"
         }
         stage("Deploy") {
             if (config.deploy) {
-                bat "npm publish"
+                sh "npm publish"
             }
         }
         body()
