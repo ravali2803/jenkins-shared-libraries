cc55ef993691d8ae925551d203ab60ba28f09e73
[ravali]git config --global user.email [ravali.anthati@gmail.com] <ravali.anthati@gmail.com>
2020-10-13 11:00:14 -0700

    updated1

--- src/org/example/constants.groovy
+++ src/org/example/constants.groovy
@@ -1,4 +1,5 @@
 package org.example
+package org.example.Constants
 class Constants {
   static final String SLACK_MESSAGE = "Sending Slack Notification" 
   static final String EMAIL_MESSAGE = "Sending Email"
